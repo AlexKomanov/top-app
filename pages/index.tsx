0ba442da99feaae508978668cf0e7ddca09dcbf2
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-01-28 11:08:41 +0200

    Adding Htag component and interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
+import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import { Htag } from '@/components'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  
+
   return (
     <div>
-  
+      <Htag tag="h1">Text</Htag>
     </div>
   )
 }
